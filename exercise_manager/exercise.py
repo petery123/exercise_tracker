126163be728b10edd9874753cc67fadb354d5eb9
petery123 <139976625+petery123@users.noreply.github.com>
2024-09-20 13:17:50 -0400

    Setup exercise class

--- exercise_manager/exercise.py
+++ exercise_manager/exercise.py
@@ -1,4 +1,23 @@
 class Exercise:
     def __init__(self, name, weight) -> None:
         self.name = name
-        self.weight = weight
+        self.weights = [weight]
+    
+    def change_name(self, new_name):
+        self.name = new_name
+    
+    def add_weight(self, weight):
+        self.weights.append(weight)
+
+    def __str__(self):
+        return f"{self.name} : {self.weights[-1]}"
+    
+    def progress(self):
+        weight_str = ""
+        for i in range(len(self.weights)):
+            if (i != len(self.weights)-1):
+                weight_str += f"{self.weights[i]}, "
+            else:
+                weight_str += f"{self.weights[i]}"
+        
+        return weight_str
