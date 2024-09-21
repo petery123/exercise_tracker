41e6935c5ddf257b777039e3336c36dd9aa7d047
petery123 <139976625+petery123@users.noreply.github.com>
2024-09-20 20:21:51 -0400

    implement session class to track session exercises

--- exercise_manager/session.py
+++ exercise_manager/session.py
@@ -1,3 +1,21 @@
+from .exercise import Exercise
+
 class Session:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, name):
+        self._name = name
+        self._exercises = []
+    
+    def change_name(self, new_name: str):
+        self._name = new_name
+    
+    def add_exercise(self, exercise: Exercise):
+        self._exercises.append(exercise)
+    
+    def remove_exercise(self, exercise_index: int):
+        return self._exercises.pop(exercise_index)
+    
+    def __str__(self) -> str:
+        workout_str = f"WORKOUT SESSION: {self._name}\n"
+        for i in range(len(self._exercises)):
+            workout_str += f"{i+1}. {(self._exercises[i])}\n"
+        return  workout_str
