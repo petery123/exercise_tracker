61a4790297144d76c682f24800013a728fe7c065
petery123 <139976625+petery123@users.noreply.github.com>
2024-09-21 13:29:06 -0400

    Add exception handling and helper funcs

--- exercise_manager/session.py
+++ exercise_manager/session.py
@@ -5,20 +5,30 @@ class Session:
         self._name = name
         self._exercises = []
     
-    def change_name(self, new_name: str):
+    def set_name(self, new_name: str):
         self._name = new_name
     
     def add_exercise(self, exercise: Exercise):
         self._exercises.append(exercise)
     
     def remove_exercise(self, exercise_index: int):
-        return self._exercises.pop(exercise_index)
+        try:
+            return self._exercises.pop(exercise_index)
+        except:
+            print("Removal Error!\n")
+            return False
     
     def __str__(self, extend=False) -> str:
         if(not extend):
            return self._name
         
         workout_str = f"WORKOUT SESSION: {self._name}"
+
+        if(not self.has_exercises()):
+            workout_str += "\nNo Workouts Added!"
         for i in range(len(self._exercises)):
             workout_str += f"\n{i+1}. {(self._exercises[i])}"
         return  workout_str
+    
+    def has_exercises(self) -> bool:
+        return (len(self._exercises) > 0)
